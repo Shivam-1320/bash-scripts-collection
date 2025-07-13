834efbc64ebd3dbb048bcb96ef82acccb703c1ab
Pankaj baghel <101424800+Shivam-1320@users.noreply.github.com>
2025-07-13 21:42:50 +0530

    Add files via upload

---
+++ Autowebdeploy-Tool/AutoWebDeploy.sh
@@ -0,0 +1,253 @@
+#!/bin/bash
+# AutoWebDeploy - Bash Web Deployment Script
+# Author: Pankaj Baghel (https://github.com/Shivam-1320)
+# License: MIT
+# Created on: July 2025
+# Description: [ ]
+
+# Please respect the open-source license.
+# Do not remove the credit if you reuse this script.
+
+
+
+#Assigning variables 
+
+DEFAULT_GITHUB_REPO="https://github.com/Shivam-1320/Test_Repo-AWS.git"
+TARGET_DIR="/var/www/html/"
+
+#Defining colour variables...
+
+GREEN="\e[32m"
+RED="\e[31m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+NC="\e[0m"
+
+#===========================================================================================================
+echo -e "${BLUE}
+    _         _     __        __   _     ____             _
+   / \  _   _| |_ __\ \      / /__| |__ |  _ \  ___ _ __ | | ___  _   _
+  / _ \| | | | __/ _ \ \ /\ / / _ \ '_ \| | | |/ _ \ '_ \| |/ _ \| | | |
+ / ___ \ |_| | || (_) \ V  V /  __/ |_) | |_| |  __/ |_) | | (_) | |_| |
+/_/   \_\__,_|\__\___/ \_/\_/ \___|_.__/|____/ \___ |.__/|_|\___/ \__, |
+                                                   |_|            |___/
+ ${NC}"
+#===========================================================================================================
+
+
+echo -e "1. ${YELLOW}Which web server do you want to install?  ${NC}" apache / nginx /
+
+read SERVER
+
+case "${SERVER,,}" in 
+
+apache)
+
+     sudo apt update -y > /dev/null 2>&1
+
+if  [ $? -eq 0 ] ; then 
+
+     echo -e ">${GREEN}System update complete${NC}"
+else
+     echo -e "${RED}System update failed! ${NC}"
+
+exit 1
+
+fi 
+
+    sudo apt install apache2 -y > /dev/null 2>&1
+
+   if [ $? -eq 0 ] ; then 
+
+   echo -e ">${GREEN}Apache Server installation successful ${NC}"
+
+else
+    echo -e ">${RED}Apache installation failed! ${NC}"
+
+    exit 1
+
+ fi
+
+    ;;
+
+nginx)
+
+     sudo apt update -y > /dev/null 2>&1
+
+if  [ $? -eq 0 ] ; then 
+
+     echo -e ">${GREEN}System update complete${NC}"
+else     
+     echo -e ">${RED}System update failed!${NC}"
+
+exit 1
+
+fi 
+
+    
+    sudo apt install nginx -y > /dev/null 2>&1
+
+if [ $? -eq 0 ] ; then 
+
+    echo -e ">${GREEN} Nginx server installation succeessful${NC}"
+else
+    echo -e ">${RED}Nginx installation failed!${NC}"
+
+    exit 1
+
+ fi
+
+ ;; 
+ 
+ *)
+    echo -e "${RED}Invalid option. Please choose apache, nginx. ${NC}"
+
+    exit 1
+    ;;
+
+esac
+
+ 
+
+    echo
+    echo
+
+    echo -e "2. ${YELLOW}Permit to clean web directory location Type ! ${NC}  Y/N "
+read ANSWER 
+
+case "${ANSWER,,}" in
+
+Y|y|yes|YES)
+
+
+sudo find "${TARGET_DIR}" -mindepth 1 -exec rm -rf {} + > /dev/null 2>&1
+
+if [ $? -eq 0 ] ; then
+
+    echo -e ">${GREEN}Your directory is successfully cleaned${NC}"
+else 
+    echo -e ">${RED}Directory cleaning operation failed! ${NC}"
+
+exit 1
+
+fi 
+
+
+    ;;
+
+N|n|no|NO) 
+
+   echo -e ">${YELLOW}Directory cleaning Process skip! ${NC}"
+
+    ;;
+
+*)
+
+echo -e "${RED}Invalid input. Please enter y or n. ${NC}"
+
+;;
+
+esac
+
+   sudo apt install git -y > /dev/null 2>&1
+
+if [ $? -eq 0 ] ; then 
+
+   echo -e ">${GREEN}Dependences installation is complete ...${NC}"
+else 
+   echo -e ">${RED}Failed to install dependences! ${NC}"
+
+   exit 1 
+
+fi 
+
+
+echo
+echo
+
+echo -e "3. ${YELLOW}Enter GitHub repo URL to deploy (or press Enter to use default Test Repo): ${NC}"
+read USER_GITHUB_REPO
+
+if [ -z "$USER_GITHUB_REPO" ] ; then
+    USER_GITHUB_REPO=$DEFAULT_GITHUB_REPO
+    
+    sudo git clone "$DEFAULT_GITHUB_REPO" "$TARGET_DIR" > /dev/null 2>&1
+
+if [ $? -eq 0 ]  ; then 
+
+    echo -e "${GREEN}Default Repository cloning is successuful${NC}"
+else    
+    echo -e "${RED} Failed to clone default repo ${NC} '/var/www/html' already exists and matbe it's not an empty directory." 
+
+    exit 1
+
+fi
+
+else
+    echo -e "${GREEN} Start Cloning your github repo${NC}$USER_GITHUB_REPO"
+
+    
+    sudo git clone "$USER_GITHUB_REPO" "$TARGET_DIR" > /dev/null 2>&1
+
+if [ $? -ne 0 ]  ; then 
+
+    echo -e "${RED} Failed to clone your repo ${NC} 'Please check your repo url is valid or don't skip cleaning directory" 
+
+    exit 1
+
+   fi    
+
+ fi
+ 
+    echo -e "${GREEN}Setting ownership to www-data${NC}"
+    sudo chown -R www-data:www-data "$TARGET_DIR" > /dev/null 2>&1
+
+    echo 
+
+    echo -e "${GREEN}Directory permissions set to 755${NC}"
+    sudo chown -R 755 "$TARGET_DIR" > /dev/null 2>&1
+
+    echo
+
+if [[ $SERVER == "apache" ]] ; then 
+
+    echo -e "${GREEN}Apache service is successfully restart${NC}"
+    sudo systemctl restart apache2.service
+ if [ $? -ne 0 ]; then
+        echo -e "${RED}Apache failed to restart!${NC}"
+        exit 1
+    fi
+
+elif [ "$SERVER" == "nginx" ]; then
+    echo -e "${GREEN}Nginx service is restarting...${NC}"
+    sudo systemctl restart nginx > /dev/null 2>&1
+    
+    
+if [ $? -ne 0 ]; then
+
+        echo -e "${RED}NGINX failed to restart!${NC}"
+        exit 1
+    fi
+fi
+
+     echo
+     echo
+     echo
+     echo -e "${BLUE}Deployment completed successfully!\e[0m"
+     echo
+     echo
+     echo -e "\e[36mScript created by: Pankaj Baghel \e[0m"
+     echo -e "\e[36mGitHub: https://github.com/Shivam-1320\e[0m"
+     echo
+     echo
+     echo
+     echo
+     echo
+
+exit 0
+
+# If you'd like to collaborate, contribute, or create new tools together
+# please feel free to contact me on GitHub.
+# I’m a beginner learning to build useful tools.  
+# Any feedback or help is greatly appreciated!
+     
---
+++ Autowebdeploy-Tool/README.md
@@ -0,0 +1,54 @@
+
+Author: Pankaj Baghel (GitHub: shivam-1320)
+
+
+# Shell scripting
+A collection of practical and beginner-friendly Bash scripts for Linux system automation, server setup, and administrative tasks. These scripts are designed to simplify common tasks such as package installation, service management, user creation, permission handling, and more. Ideal for sysadmins, DevOps learners, and anyone looking to streamline their terminal workflow.
+
+
+
+**Features**
+
+Install and configure services like Apache, Nginx, etc.
+
+Automated system updates and cleanup
+
+User account creation with permission settings
+
+Service status checking and conditional logic
+
+Output messages with color formatting for better visibility
+
+
+
+
+**⚠ Disclaimer**
+
+Please do not blindly trust or run these scripts in production environments.
+I’m a beginner still learning Bash scripting, and this repository is a work in progress.
+I frequently update and revise these scripts — mistakes are possible, and things may break.
+Always review and test scripts in a safe environment before using them on real systems.
+
+
+
+ 
+**Requirements**
+
+Linux system (Ubuntu, Debian, Kali, etc.)
+
+bash shell
+
+Root privileges (for most scripts)
+
+
+ 
+ **Credits**
+ 
+  Created by Pankaj Baghel
+
+## Contact
+
+You can reach me on GitHub by opening an issue in this repository,  
+or visit my profile: (https://github.com/shivam-1320)  
+
+Feel free to send me a message or create an issue for collaboration and questions.
